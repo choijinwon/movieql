6c47619bf5f2803ea657f0cdf3aa26d260980014
최진원 <choejin-won@choejin-won-ui-MacBookPro.local>
2019-10-12 21:44:44 +0900

    api Test get

---
+++ graphql/db.js
@@ -0,0 +1,37 @@
+export const people = [
+    {
+        id:"0",
+        name:"chojinwon",
+        age:"37",
+        gender:"female"
+    },
+    {
+        id:"1",
+        name:"jinwon",
+        age:"27",
+        gender:"female"
+    },
+    {
+        id:"2",
+        name:"babo",
+        age:"28",
+        gender:"female"
+    },
+    {
+        id:"3",
+        name:"namek",
+        age:"29",
+        gender:"female"
+    },
+    {
+        id:"4",
+        name:"chojinwon",
+        age:"37",
+        gender:"female"
+    },
+    ];
+    export const getById = id =>{
+        const filteredPeople = people.filter(person => person.id ===String(id));
+        return filteredPeople[0];
+    }
+    
