4b5cbd6233574967c20675de3e0b0096ca7b5de6
최진원 <choejin-won@choejin-won-ui-MacBookPro.local>
2019-10-12 23:05:08 +0900

    api update

--- graphql/db.js
+++ graphql/db.js
@@ -1,37 +1,57 @@
-export const people = [
+export const movies= [
     {
         id:"0",
-        name:"chojinwon",
-        age:"37",
-        gender:"female"
+        name:"Start Wars",
+        score:"1",
     },
     {
         id:"1",
-        name:"jinwon",
-        age:"27",
-        gender:"female"
+        name:"Start Wars-2",
+        score:"2",
     },
     {
         id:"2",
-        name:"babo",
-        age:"28",
-        gender:"female"
+        name:"Start War-3",
+        score:"3",
     },
     {
         id:"3",
-        name:"namek",
-        age:"29",
-        gender:"female"
+        name:"Start Wars",
+        score:"4",
     },
     {
         id:"4",
-        name:"chojinwon",
-        age:"37",
-        gender:"female"
+        name:"Start Wars",
+        score:"5",
+    },
+    {
+        id:"5",
+        name:"hello",
+        score:"6",
     },
     ];
-    export const getById = id =>{
-        const filteredPeople = people.filter(person => person.id === String(id));
-        return filteredPeople[0];
+
+    export const getMovies = ()=> movies;
+
+    export const getById = id => {
+        const filteredMovies = movies.filter(movie => movie.id === String(id));
+        return filteredMovies[0];
     }
-    
+    export const deletMovie = (id) => {
+        const cleanedMovies = movies.filter(movie=>movie.id !== String(id));
+        if(movies.length > cleanedMovies.length ){
+            movie = cleanedMovies;
+            return true;
+        }else{
+            return false;
+        }
+    }
+    export const addMovie = (name,score) => {
+        const newMovie = {
+            id:`${movies.length + 1}`,
+            name,
+            score
+        }
+        movies.push(newMovie);
+        return newMovie;
+    }
